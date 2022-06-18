b97da43e4a9abe95efa8a38767bca129517aaa61
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-06-18 19:33:10 +0200

    Buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import type { NextPage } from 'next';
 import { getOptionsForVote } from '../util/getRandomPokemon';
 import trpc from '../util/trpc';
 
+const btn =
+  'inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500';
+
 const Home: NextPage = () => {
   const [ids, updateIds] = useState(() => getOptionsForVote());
   const [first, second] = ids;
@@ -11,6 +14,12 @@ const Home: NextPage = () => {
 
   if (firstPokemon.isLoading || secondPokemon.isLoading)
     return <div>loading...</div>;
+
+  function voteForRoundest(selected: number) {
+    // todo: fire mutation to persist changes
+
+    updateIds(getOptionsForVote());
+  }
 
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center'>
@@ -22,6 +31,9 @@ const Home: NextPage = () => {
           <div className='text-xl text-center capitalize mt-[-2rem]'>
             {firstPokemon.data?.name}
           </div>
+          <button className={btn} onClick={() => voteForRoundest(first)}>
+            Rounder
+          </button>
         </div>
         <div className='p-8'>vs</div>
         <div className='w-64 h-64 flex flex-col align-middle items-center '>
@@ -29,6 +41,9 @@ const Home: NextPage = () => {
           <div className='text-xl text-center capitalize mt-[-2rem]'>
             {secondPokemon.data?.name}
           </div>
+          <button className={btn} onClick={() => voteForRoundest(second)}>
+            Rounder
+          </button>
         </div>
         <div className='p-2' />
       </div>
