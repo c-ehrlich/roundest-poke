771dee774af5c229ebec4a76858b11eff0cfcbbf
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-06-18 20:14:36 +0200

    Fix localhost baseurl

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ function getBaseUrl() {
   if (process.browser) return ''; // Browser should use correct path
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
 
-  return `httpL//localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 }
 
 export default withTRPC<AppRouter>({
