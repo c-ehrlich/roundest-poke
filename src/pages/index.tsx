7ea87a64af345d359b93ade30c22332415be15aa
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-06-18 19:14:00 +0200

    Don't use SSR for now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,46 @@
+import { useState } from 'react';
 import type { NextPage } from 'next';
 import { getOptionsForVote } from '../util/getRandomPokemon';
 import trpc from '../util/trpc';
+import Image from 'next/image';
 
-interface Props {
-  first: number;
-  second: number;
-}
-
-const Home: NextPage<Props> = ({ first, second }: Props) => {
+const Home: NextPage = () => {
+  const [ids, updateIds] = useState(() => getOptionsForVote());
+  const [first, second] = ids;
   const firstPokemon = trpc.useQuery(['get-pokemon-by-id', { id: first }]);
   const secondPokemon = trpc.useQuery(['get-pokemon-by-id', { id: second }]);
 
-  console.log(firstPokemon.data);
+  if (firstPokemon.isLoading || secondPokemon.isLoading)
+    return <div>loading...</div>;
 
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center'>
       <div className='text-2xl text-center'>Which Pokémon is Roundest?</div>
       <div className='p-2' />
       <div className='border rounded p-8 max-w-2xl flex justify-between items-center'>
-        <div className='w-16 h-16 bg-red-800'>{first}</div>
+        <div className='w-64 h-64 flex flex-col align-middle items-center'>
+          <img
+            src={firstPokemon.data?.sprites.front_default!}
+            className='w-full'
+          />
+          <div className='text-xl text-center capitalize mt-[-2rem]'>
+            {firstPokemon.data?.name}
+          </div>
+        </div>
         <div className='p-8'>vs</div>
-        <div className='w-16 h-16 bg-red-800'>{second}</div>
+        <div className='w-64 h-64 flex flex-col align-middle items-center '>
+          <img
+            src={secondPokemon.data?.sprites.front_default!}
+            className='w-full'
+          />
+          <div className='text-xl text-center capitalize mt-[-2rem]'>
+            {secondPokemon.data?.name}
+          </div>
+        </div>
+        <div className='p-2' />
       </div>
     </div>
   );
 };
 
 export default Home;
-
-export async function getServerSideProps() {
-  const [first, second] = getOptionsForVote();
-
-  return {
-    props: { first, second },
-  };
-}
