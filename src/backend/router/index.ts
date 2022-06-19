4dde5a5f4be1e4755d9291f6072640cbb62f746a
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-06-19 16:56:40 +0200

    Add voting mutation

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,18 +1,35 @@
 import * as trpc from '@trpc/server';
 import { PokemonClient } from 'pokenode-ts';
+import { prisma } from '../utils/prisma';
 
 import { z } from 'zod';
 
-export const appRouter = trpc.router().query('get-pokemon-by-id', {
-  input: z.object({
-    id: z.number().min(1).max(493),
-  }),
-  async resolve({ input }) {
-    const api = new PokemonClient();
-    const pokemon = await api.getPokemonById(input.id);
-    return { name: pokemon.name, sprite: pokemon.sprites.front_default };
-  },
-});
+export const appRouter = trpc
+  .router()
+  .query('get-pokemon-by-id', {
+    input: z.object({
+      id: z.number().min(1).max(493),
+    }),
+    async resolve({ input }) {
+      const api = new PokemonClient();
+      const pokemon = await api.getPokemonById(input.id);
+      return { name: pokemon.name, sprite: pokemon.sprites.front_default };
+    },
+  })
+  .mutation('cast-vote', {
+    input: z.object({
+      votedFor: z.number(),
+      votedAgainst: z.number(),
+    }),
+    async resolve({ input }) {
+      const voteInDb = await prisma.vote.create({
+        data: {
+          ...input,
+        },
+      });
+      return { success: true, vote: voteInDb };
+    },
+  });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
