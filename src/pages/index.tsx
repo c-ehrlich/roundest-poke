fb84c27c6319a032aeb2b509bc7b3d0565c1462e
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-06-20 10:05:48 +0200

    Landing page style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const btn =
-  'inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500';
+  'inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 -mt-4';
 
 const Home: NextPage = () => {
   const [ids, updateIds] = useState(() => getOptionsForVote());
@@ -35,12 +35,12 @@ const Home: NextPage = () => {
       firstPokemon.data &&
       !secondPokemon.isLoading &&
       secondPokemon.data ? (
-        <div className='border rounded p-8 max-w-2xl flex justify-between items-center'>
+        <div className='rounded p-8 max-w-2xl flex flex-col sm:flex-row justify-between items-center'>
           <PokemonListing
             pokemon={firstPokemon.data}
             vote={() => voteForRoundest(first)}
           />
-          <div className='p-8'>vs</div>
+          <div className='p-8 text-lg italic'>or</div>
           <PokemonListing
             pokemon={secondPokemon.data}
             vote={() => voteForRoundest(second)}
@@ -49,7 +49,7 @@ const Home: NextPage = () => {
       ) : (
         <div>Loading...</div>
       )}
-      <div className='sticky bottom-0 w-full mb-8 text-xl flex gap-4 justify-center'>
+      <div className='bottom-0 w-full p-4 text-xl flex gap-4 justify-center'>
         <Link href='/results'>
           <a>Results</a>
         </Link>
@@ -69,7 +69,7 @@ const PokemonListing: FC<{ pokemon: PokemonFromServer; vote: () => void }> = (
 ) => {
   return (
     <div className='flex flex-col align-middle items-center'>
-      <div className='text-xl text-center capitalize mt-[-2rem]'>
+      <div className='text-xl text-center capitalize mb-[-2rem] sm:m-0'>
         {props.pokemon.name}
       </div>
       <Image
