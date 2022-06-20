e2ad116baca44d4987baa47ff35dcc446c38a17c
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-06-20 10:15:34 +0200

    Make button not overlap pokemon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const btn =
-  'inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 -mt-4';
+  'inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 -mt-1';
 
 const Home: NextPage = () => {
   const [ids, updateIds] = useState(() => getOptionsForVote());
