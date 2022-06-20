f7cdd375a723c49ec0f5570ef338aea956438b70
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-06-20 09:27:08 +0200

    Results page

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -1,5 +1,84 @@
-function ResultsPage() {
-  return <div className='text-2xl'>results: todo</div>;
-}
+import { GetStaticProps } from 'next';
+import Image from 'next/image';
+import { prisma } from '../backend/utils/prisma';
+import { AsyncReturnType } from '../utils/inferType';
+
+const ONE_MINUTE = 1 * 60;
+
+type PokemonQueryResult = AsyncReturnType<typeof getPokemonInOrder>;
+
+// TODO sort em my own way (by percentage)
+
+const PokemonResultListing: React.FC<{
+  pokemon: PokemonQueryResult[number];
+  index: number;
+}> = (props) => {
+  return (
+    <div className='flex border-white border relative w-full max-w-2xl justify-between items-center'>
+      <div className='flex items-center gap-2'>
+        <div className='w-10 h-10 bg-gray-700 flex flex-col place-self-start justify-center items-center rounded-br-2xl text-xl'>
+          {props.index}
+        </div>
+        <Image
+          className='-m-8 -p-8'
+          src={props.pokemon.spriteUrl}
+          alt={props.pokemon.name}
+          width={96}
+          height={96}
+          layout='fixed'
+        />
+        <div className='text-xl'>{props.pokemon.name}</div>
+      </div>
+      <div className='mr-4 flex flex-col'>
+        <div className='self-end'>For: {props.pokemon._count.votesFor}</div>
+        <div className='self-end'>
+          Against: {props.pokemon._count.votesAgainst}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ResultsPage: React.FC<{
+  pokemon: PokemonQueryResult;
+}> = (props) => {
+  return (
+    <div className='flex flex-col w-screen items-center'>
+      <h1 className='text-2xl m-4'>Results</h1>
+      {props.pokemon.map((pokemon, index) => (
+        <PokemonResultListing
+          key={pokemon.id}
+          pokemon={pokemon}
+          index={index + 1}
+        />
+      ))}
+    </div>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const pokemonOrdered = await getPokemonInOrder();
+  return {
+    props: { pokemon: pokemonOrdered },
+    // revalidate: ONE_MINUTE,
+  };
+};
+
+const getPokemonInOrder = async () => {
+  return await prisma.pokemon.findMany({
+    orderBy: { votesFor: { _count: 'desc' } },
+    select: {
+      id: true,
+      name: true,
+      spriteUrl: true,
+      _count: {
+        select: {
+          votesFor: true,
+          votesAgainst: true,
+        },
+      },
+    },
+  });
+};
 
 export default ResultsPage;
