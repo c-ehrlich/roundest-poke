444c78a1191b8d4f6db07c10c26f25cb38929897
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-06-20 10:15:02 +0200

    Create about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,24 @@
+import Link from 'next/link';
+
 function AboutPage() {
-  return <div className='text-2xl'>about: todo</div>;
+  return (
+    <div>
+      <Link href='/' className='fixed left-0 top-0 text-xl'>
+        <a>{'<'} Back</a>
+      </Link>
+      <div className='w-screen h-screen relative flex flex-col items-center justify-center'>
+        <h1 className='text-2xl mb-2'>Roundest Pokemon</h1>
+        <p>I made this to learn about tRPC and Tailwind.</p>
+        <p>
+          Most of it is stolen from{' '}
+          <Link href='https://t3.gg/'>
+            <a className='text-gray-400'>Theo</a>
+          </Link>
+          .
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default AboutPage;
